e707d20ad69d57be0a40f45ff734fa55bd354bd7
Paul Ccari <paulclindo@gmail.com>
2020-09-03 13:11:54 -0500

    added a home hero

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import heroImg from "../assets/hero.svg";
 
 const Wrapper = styled.div`
   background: linear-gradient(to right, #11998e, #38ef7d);
@@ -20,6 +21,14 @@ const Wrapper = styled.div`
 function HomeContainer(props) {
   return (
     <Wrapper>
+      <h1>Welcome to Chateable</h1>
+      <p>
+        Our platform is intented for developers and designers who want to share
+        anything to our community. Join us!
+      </p>
+      <div>
+        <img src={heroImg} alt="" />
+      </div>
       <Link to="/login">Log In</Link>
       <Link to="/login">Sign Up</Link>
     </Wrapper>
